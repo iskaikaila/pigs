77dc6881acd64c2b9e6bddfca8020f65a17bab15
Irene Chen <104665922+iskaikaila@users.noreply.github.com>
2023-12-13 15:06:08 -0800

    Update pigService.ts

--- cmpt272-A3/pigService.ts
+++ cmpt272-A3/pigService.ts
@@ -10,6 +10,7 @@ export class PigService {
     private loadPigs(): void {
         const storedPigs = localStorage.getItem('pigs');
         if (storedPigs) {
+            // 从本地存储加载猪的数据，并将其转换为Pig对象数组
             this.pigs = JSON.parse(storedPigs).map((pigData: IPig) => new Pig(
                 pigData.name,
                 pigData.breed,
@@ -25,41 +26,47 @@ export class PigService {
             ));
         }
 
-        console.log('Loaded pigs:', this.pigs); // 调试输出
+        console.log('Loaded pigs:', this.pigs); // 调试输出，显示加载的猪
     }
+
     private savePigs(): void {
+        // 将猪的数据保存到本地存储中
         localStorage.setItem('pigs', JSON.stringify(this.pigs));
     }
 
     addPig(pig: Pig): void {
+        // 添加一头猪到猪列表，并保存猪的数据
         this.pigs.push(pig);
         this.savePigs(); 
     }
 
     getPigs(): Pig[] {
+        // 返回猪列表
         return this.pigs;
     }
 
+    
     deletePig(pigName: string): void {
+        // 根据猪的名字删除猪
         this.pigs = this.pigs.filter(pig => pig.name !== pigName);
-        this.savePigs();
+        this.savePigs(); // 保存更新后的猪列表
     }
 
-
-
+    //分组
     getPigsGroupedByCategory(): Record<PigCategory, Pig[]> {
-      const grouped = this.pigs.reduce((acc, pig) => {
-          (acc[pig.category] = acc[pig.category] || []).push(pig);
-          return acc;
-      }, {} as Record<PigCategory, Pig[]>);
+        // 将猪按照类别分组，并按照猪的名字排序
+        const grouped = this.pigs.reduce((acc, pig) => {
+            (acc[pig.category] = acc[pig.category] || []).push(pig);
+            return acc;
+        }, {} as Record<PigCategory, Pig[]>);
 
-      // Correctly typed loop for sorting
-      for (const category in grouped) {
-          if (grouped.hasOwnProperty(category)) {
-              grouped[category as PigCategory].sort((a, b) => a.name.localeCompare(b.name));
-          }
-      }
+        // 正确类型化的循环，用于对分组后的猪进行排序
+        for (const category in grouped) {
+            if (grouped.hasOwnProperty(category)) {
+                grouped[category as PigCategory].sort((a, b) => a.name.localeCompare(b.name));
+            }
+        }
 
-      return grouped;
+        return grouped; // 返回分组后的猪
     }
 }
